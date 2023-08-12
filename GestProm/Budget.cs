8a993ce0900a6a86998dd6e7010d15d2b260b612
Joserafpg <payerogarciaj@gmail.com>
2023-08-12 10:44:36 -0400

    Version 2.0

--- GestProm/Budget.cs
+++ GestProm/Budget.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace GestProm
 {
@@ -15,6 +16,39 @@ namespace GestProm
         public Budget()
         {
             InitializeComponent();
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            panelgeneral.Visible = true;
+            panelnew.Visible = true;
+        }
+
+        private void btnbudgetname_Click(object sender, EventArgs e)
+        {
+            string texto = budgetname.Text;
+            if (texto.Count() > 4 )
+            {
+                panelbudget.Visible = true;
+            }
+
+            else
+            {
+                if (texto.Count() > 1) 
+                {
+                    MessageBox.Show("The name is too short");              
+                }
+
+                else
+                {
+                    MessageBox.Show("Please write a name");
+                }
+            }
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
